abc75d987f641d4f0fd7f9b67c44bd0eca7372bd
Vipul Patel <vipul.patel@in.insigmainc.com>
2017-03-08 12:49:37 +0530

    Tracker: 3344
    # Resaon

--- main.c
+++ main.c
@@ -2,8 +2,9 @@
 #include <conio.h>
 int main(void)
 {
-	printf("Git IntrimV1.1.0");
+	printf("Git IntrimV1.2.0");
 	printf("Git Intrim Tracker:5678");
 	printf("Git Intrim Tracker:1122");
+	printf("Git Intrim Tracker:3344");
 	return 0;
 }
