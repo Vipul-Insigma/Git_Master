7e7ac1ab65a0f149b1592cc8bc66bc0b84720c00
Vipul Patel <vipul.patel@in.insigmainc.com>
2017-03-08 12:50:31 +0530

    Tracker: 5566
    # Reason

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 int main(void)
 {
 	printf("Git IntrimV1.2.0");
-	printf("Git Intrim Tracker:5678");
 	printf("Git Intrim Tracker:1122");
 	printf("Git Intrim Tracker:3344");
+	printf("Git Intrim Tracker:5566");
 	return 0;
 }
